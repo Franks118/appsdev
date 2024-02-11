59880a60154b88e84ed785f4377e9c65d5fe0540
Franks118 <111149236+Franks118@users.noreply.github.com>
2024-02-11 21:36:59 +0800

    Add files via upload

---
+++ ifelse(windows)/ifelse(windows)/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ifelse_windows_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double discountedPrice;
+            double purchaseAmount = Convert.ToDouble(textBox1.Text);
+
+            double discountRate = 0.10; // 10% discount rate
+
+
+
+            if (1000 >= purchaseAmount)
+            {
+                //if its less than 1000
+                textBox2.Text =(" Discount: No Discount ");
+            }
+            else
+            {
+                // Apply the discount if the purchase amount is greater than 1000
+                discountedPrice = purchaseAmount * discountRate;
+                textBox2.Text = ($"Discounted price: {discountedPrice:N2}");
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
