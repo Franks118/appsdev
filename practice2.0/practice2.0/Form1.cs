b90e4ced291ed8217885d0ff5709b084fc33623b
Franks118 <111149236+Franks118@users.noreply.github.com>
2024-02-20 23:36:30 +0800

    Add files via upload

---
+++ practice2.0/practice2.0/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practice2._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num = 0;
+            num = Convert.ToInt32(textBox1.Text);//to get the input
+            preview ui = new preview();//connecting the page
+            ui.setvalues(num);//getting the value from here and sending it to the second page
+            ui.Show();//show
+        }
+    }
+}
---
+++ practice2.0/practice2.0/preview.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practice2._0
+{
+    public partial class preview : Form
+    {
+        public preview()
+        {
+            InitializeComponent();
+        }
+        public void setvalues(int num) {
+            String pop = "Gay";
+            String pop1 = "not Gay";
+            int total = 0;
+            for (int i = 0; i < 3; i++) { 
+            total += num;
+                label1.Text =   total.ToString("N2");
+                String oi = "Php"+total.ToString("c");
+                label2.Text = oi;
+            }
+            if (num >= 1000)
+            {
+
+                label3.Text = pop;
+
+
+            }
+            else {
+
+                label3.Text = pop1;
+
+            }
+
+
+        }
+    }
+}
